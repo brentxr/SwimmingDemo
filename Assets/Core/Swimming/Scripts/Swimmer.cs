eb76553dae49e0e6cfa8445b3f07953a9ee6f92f
Brent <100012424+brentxr@users.noreply.github.com>
2022-08-22 02:23:52 -0400

    integrated VRIF

--- Assets/Core/Swimming/Scripts/Swimmer.cs
+++ Assets/Core/Swimming/Scripts/Swimmer.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
 
 [RequireComponent(typeof(Rigidbody))]
 public class Swimmer : MonoBehaviour
