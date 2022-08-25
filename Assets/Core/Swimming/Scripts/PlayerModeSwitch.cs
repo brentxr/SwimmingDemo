5069aa7f86f73a4c5f46f014e3dff05ea54d71e7
Brent <100012424+brentxr@users.noreply.github.com>
2022-08-25 00:36:56 -0400

    finished demo with wrist menu and teleporting

--- Assets/Core/Swimming/Scripts/PlayerModeSwitch.cs
+++ Assets/Core/Swimming/Scripts/PlayerModeSwitch.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using BNG;
 using UnityEngine;
+using UnityEngine.UI;
+using Button = UnityEngine.UI.Button;
 
 public class PlayerModeSwitch : MonoBehaviour
 {
@@ -9,6 +11,8 @@ public class PlayerModeSwitch : MonoBehaviour
     [SerializeField] private PlayerTeleport playerTeleport;
     [SerializeField] private PlayerGravity playerGravity;
     [SerializeField] private Swimmer swimmer;
+    [SerializeField] private GameObject backToBaseButton;
+    [SerializeField] private ScreenFader screenFader;
     
 
     [SerializeField] private Transform waterStartLocation;
@@ -19,21 +23,30 @@ public class PlayerModeSwitch : MonoBehaviour
 
     public void ToggleSwimmer()
     {
+        screenFader = FindObjectOfType<ScreenFader>();
 
+        screenFader.DoFadeIn();
         playerTeleport.TeleportPlayerToTransform(waterStartLocation);
         rb.useGravity = false;
         swimmer.enabled = true;
         playerGravity.enabled = false;
-       
+        screenFader.DoFadeOut();
+        //backToBaseButton.GetComponent<Button>().enabled = true;
+        //backToBaseButton.GetComponent<Image>().enabled = true;
+
     }
     
     public void ToggleBackToBase()
     {
-        
+        screenFader = FindObjectOfType<ScreenFader>();
+
+        screenFader.DoFadeIn();
         playerTeleport.TeleportPlayerToTransform(baseEntranceLocation);
         rb.useGravity = true;
         swimmer.enabled = false;
         playerGravity.enabled = true;
-
+        screenFader.DoFadeOut();
+        //backToBaseButton.GetComponent<Button>().enabled = false;
+        //backToBaseButton.GetComponent<Image>().enabled = false;
     }
 }
